ec72a3e13eae4d14515c04b42a860cf1a643051f
dvrkdev <dvrk.devx@gmail.com>
2025-12-06 11:03:00 +0500

    feat: find elements

--- main.py
+++ main.py
@@ -2,19 +2,56 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # https://sites.google.com/chromium.org/driver
 service = Service(executable_path='chromedriver-linux64/chromedriver')
 
+options = Options()
+options.headless = True
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.page_load_strategy = 'eager'
+
 # Chrome brauzerini ishga tushirish
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(options=options, service=service)
+# driver = webdriver.Chrome(options=options)
 
 # Veb-sahifaga o'tish
-driver.get('https://google.com')
+driver.get('https://techbeamers.com/selenium-practice-test-page/')
+# driver.get('https://instagram.com')
 
 # Sahifa sarlavhasini chop etish
 print(f'Sahifa sarlavhasi: {driver.title}')
+
+# # ID bo'yicha element topish
+# element_by_id = driver.find_element(By.ID, 'username')
+# element_by_id.send_keys('dvrkdev')
+
+# # Name bo'yicha element topish
+# element_by_name = driver.find_element(By.NAME, 'username')
+# element_by_name.send_keys('dvrkdev')
+
+# Class nomi bo'yicha element topish
+# element_by_class = driver.find_element(By.CLASS_NAME, 's-title')
+# print(element_by_class.text)
+
+# # XPath bo'yicha element topish
+# element_by_xpath = driver.find_element(By.XPATH, '//input[@type="email"]')
+# print(element_by_xpath)
+
+# # CSS selector bo'yicha element topish
+# element_by_css = driver.find_element(By.CSS_SELECTOR, 'a.meta-author')
+# print(element_by_css.text)
+
+username = WebDriverWait(driver, 10).until(
+	EC.presence_of_element_located((By.ID, 'username')))
+username.send_keys('dvrkdev')
 
 # 5 soniya kutish
 time.sleep(5)
