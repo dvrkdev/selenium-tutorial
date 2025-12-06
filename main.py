2ddf0c57434f71c53538c94dc40dbb18eebf21b5
dvrkdev <dvrk.devx@gmail.com>
2025-12-06 09:44:54 +0500

    feat: initial program

--- main.py
+++ main.py
@@ -2,20 +2,22 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 # https://sites.google.com/chromium.org/driver
 service = Service(executable_path='chromedriver-linux64/chromedriver')
+
+# Chrome brauzerini ishga tushirish
 driver = webdriver.Chrome(service=service)
 
+# Veb-sahifaga o'tish
 driver.get('https://google.com')
 
-input_element = driver.find_element(By.NAME, 'q')
-input_element.send_keys('Diyorbek Qodirboyev' + Keys.ENTER)
+# Sahifa sarlavhasini chop etish
+print(f'Sahifa sarlavhasi: {driver.title}')
 
+# 5 soniya kutish
+time.sleep(5)
 
-time.sleep(100)
-
+# Brauzerni yopish
 driver.quit()
